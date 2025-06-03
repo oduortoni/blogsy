9854b974a7f72888805098c56089123ebbd4287e
oduortoni <oduortoni@gmail.com>
2025-06-04 00:35:09 +0300

    feat: add fallback route
    add a fallback route to the web.php file
    this will allow us to navigate to any route that is not defined

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,4 @@ use App\Http\Controllers\SPAController;
 use Illuminate\Support\Facades\Route;
 
 /* will serve our SPA index.html file */
-Route::get('/', [SPAController::class, 'index'])->name('index');
-Route::fallback(function () {
-    return view('index');
-});
+Route::get('/{any}', [SPAController::class, 'index'])->where('any', '.*');
